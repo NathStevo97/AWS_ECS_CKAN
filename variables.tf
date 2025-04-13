d4eed6635aa7c6dd9ae9fe8839ad531daecb5e75
NathStevo97 <natstephenson15@gmail.com>
2025-04-13 21:45:02 +0100

    chore: provider updates, tidying, and testing modules other than ckan-cluster work

--- variables.tf
+++ variables.tf
@@ -1,17 +1,25 @@
 variable "resource_name_prefix" {
   type = string
+  default = "ckan"
 }
 
 variable "vpc_cidr" {
   type = string
+  default = ""
 }
 
 variable "region" {
   type = string
+  default = "eu-west-2"
 }
 
 variable "availability_zone_map" {
   type = map(any)
+  default = {
+    "eu-west-2a" = "eu-west-2a"
+    "eu-west-2b" = "eu-west-2b"
+    "eu-west-2c" = "eu-west-2c"
+  }
 }
 
 variable "domain_name" {
@@ -26,37 +34,45 @@ variable "hosted_zone_id" {
 
 variable "rds_database_name" {
   type = string
+  default = "ckan"
 }
 
 variable "rds_database_username" {
   type = string
+  default = "ckan"
 }
 
 variable "rds_database_password" {
   type = string
+  default = "ckan"
 }
 
 variable "instance_class" {
   type        = string
   description = "Database instance type e.g. db.t2.micro, this can be adjusted to suit using the options at https://aws.amazon.com/rds/instance-types/"
+  default     = "db.t2.micro"
 }
 
 
 
 variable "admin_cidr_blocks" {
   type = string
+  default = ""
 }
 
 variable "rds_readonly_database_name" {
   type = string
+  default = "ckan_readonly"
 }
 
 variable "rds_readonly_database_user" {
   type = string
+  default = "readonly"
 }
 
 variable "rds_readonly_database_password" {
   type = string
+  default = "readonly"
 }
 /*
 variable "ckan_admin" {
@@ -71,4 +87,5 @@ variable "ckan_admin_password" {
 
 variable "lb_acm_certificate_arn" {
   type = string
+  default = ""
 }
